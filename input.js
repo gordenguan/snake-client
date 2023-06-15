da1ccf48287e4ff1df01057e11480a27ff5055c7
Gordenguan <gordenguan122@gmail.com>
2023-06-15 00:15:15 -0600

    create input.js and export function to play.js

---
+++ input.js
@@ -0,0 +1,17 @@
+//use process.stdin to receive client type data
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin;
+};
+//terminate with ctrl + C
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+module.exports = {setupInput}
--- play.js
+++ play.js
@@ -1,20 +1,8 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input')
 
+console.log('Connecting...')
 connect();
-//use process.stdin to receive client type data
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput)
-  return stdin;
-};
-//terminate with ctrl + C
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
+
 
 setupInput();
