ec11bea67bfb7518b2858e1244a57ec9b055e480
Gordenguan <gordenguan122@gmail.com>
2023-06-14 20:44:59 -0600

    create client.js, export function to play.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '192.168.0.21',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Connecting ...");
+    conn.on('data', () => {
+      console.log('you ded cuz you idled');
+    });
+  });
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,23 +1,3 @@
-const net = require('net');
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '192.168.0.21',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('connect', () => {
-    console.log("Connecting ...");
-    conn.on('data', () => {
-      console.log('you ded cuz you idled');
-    });
-  });
-
-  return conn;
-};
+const connect = require('./client');
 
 connect();
