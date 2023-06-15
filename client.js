2db49d09116b20389516a3bde0482d871d5f6cab
Gordenguan <gordenguan122@gmail.com>
2023-06-14 21:47:04 -0600

    add settimeout and setinterval with moves then comment out

--- client.js
+++ client.js
@@ -12,11 +12,22 @@ const connect = function() {
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write('Name: NIC')
+    // let moves = ['Move: up', 'Move: right', 'Move: up', 'Move: right'];
+    // let delay = 100;
+    // for (let move of moves) {
+    //   setTimeout(() => {
+    //     conn.write(move)  
+    //   }, delay)
+    //   delay += 250;
+    // }
+    // setInterval(() => {
+    //   conn.write('Move: up')
+    // }, 550)
     conn.on('data', () => {
-      console.log('you ded cuz you idled');
-    });
-  });
-
+      console.log('you ded cuz you idled')
+    })
+  })
+  
   return conn;
 };
 
